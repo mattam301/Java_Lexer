4d27e7a6e4e3332a94408c7ef29e69fd83a8b6c3
thanhnt3 <thanhnt3@adflex.vn>
2023-05-11 12:08:05 +0700

    Lexer: Fixed type of grammar

--- app/src/main/java/LL1Parser/ParseTable.java
+++ app/src/main/java/LL1Parser/ParseTable.java
@@ -6,38 +6,37 @@ public class ParseTable {
 
     private Map<String, Set<String>> firstSets;
     private Map<String, Set<String>> followSets;
-    private Map<String, List<String>> productions;
-    private Map<String, Map<String, String>> parseTable;
+    private Map<String, List<List<String>>> grammar;
+    private Map<String, Map<String, List<String>>> parseTable;
 
     public ParseTable(Map<String, Set<String>> firstSets, Map<String, Set<String>> followSets,
-                      Map<String, List<String>> productions) {
+                      Map<String, List<List<String>>> grammar) {
         this.firstSets = firstSets;
         this.followSets = followSets;
-        this.productions = productions;
+        this.grammar = grammar;
         this.parseTable = new HashMap<>();
     }
 
     public void generateParseTable() {
-        for (String nonTerminal : firstSets.keySet()) {
+        for (String nonTerminal : grammar.keySet()) {
             parseTable.put(nonTerminal, new HashMap<>());
-            for (String production : productions.get(nonTerminal)) {
-                String[] symbols = production.split(" ");
-                String firstSymbol = symbols[0];
+            for (List<String> production : grammar.get(nonTerminal)) {
+                String firstSymbol = production.get(0);
                 if (firstSets.containsKey(firstSymbol)) {
                     for (String terminal : firstSets.get(firstSymbol)) {
                         if (!terminal.equals("epsilon")) {
-                            parseTable.get(nonTerminal).put(terminal, nonTerminal + " -> " + production);
+                            parseTable.get(nonTerminal).put(terminal, production);
                         } else {
                             for (String follow : followSets.get(nonTerminal)) {
-                                parseTable.get(nonTerminal).put(follow, nonTerminal + " -> epsilon");
+                                parseTable.get(nonTerminal).put(follow, Arrays.asList("epsilon"));
                             }
                         }
                     }
                 } else if (!firstSymbol.equals("epsilon")) {
-                    parseTable.get(nonTerminal).put(firstSymbol, nonTerminal + " -> " + production);
+                    parseTable.get(nonTerminal).put(firstSymbol, production);
                 } else {
                     for (String follow : followSets.get(nonTerminal)) {
-                        parseTable.get(nonTerminal).put(follow, nonTerminal + " -> epsilon");
+                        parseTable.get(nonTerminal).put(follow, Arrays.asList("epsilon"));
                     }
                 }
             }
@@ -67,12 +66,18 @@ public class ParseTable {
         followSets.put("F", Set.of("$", ")", "*", "+", "-", "/"));
 
         // Sample production rules
-        Map<String, List<String>> productions = new HashMap<>();
-        productions.put("E", Arrays.asList("T E'"));
-        productions.put("E'", Arrays.asList("+ T E'", "- T E'", "epsilon"));
-        productions.put("T", Arrays.asList("F T'"));
-        productions.put("T'", Arrays.asList("* F T'", "/ F T'", "epsilon"));
-        productions.put("F", Arrays.asList("( E )", "id", "num"));
+        Map<String, List<List<String>>> productions = new HashMap<>();
+        productions.put("E", List.of(Arrays.asList("T", "E'")));
+        productions.put("E'", Arrays.asList(Arrays.asList("+", "T", "E'"),
+                Arrays.asList("-", "T", "E'"),
+                Collections.singletonList("epsilon")));
+        productions.put("T", List.of(Arrays.asList("F", "T'")));
+        productions.put("T'", Arrays.asList(Arrays.asList("*", "F", "T'"),
+                Arrays.asList("/", "F", "T'"),
+                Collections.singletonList("epsilon")));
+        productions.put("F", Arrays.asList(Arrays.asList("(", "E", ")"),
+                Collections.singletonList("id"),
+                Collections.singletonList("num")));
 
         ParseTable parseTable = new ParseTable(firstSets, followSets, productions);
         parseTable.generateParseTable();
