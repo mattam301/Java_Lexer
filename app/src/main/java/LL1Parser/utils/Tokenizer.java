39bcb7da0c144f53c30e311a7708c16a0f017e9f
thanhnt3 <thanhnt3@adflex.vn>
2023-05-23 13:24:14 +0700

    Finalize

--- app/src/main/java/LL1Parser/utils/Tokenizer.java
+++ app/src/main/java/LL1Parser/utils/Tokenizer.java
@@ -11,7 +11,46 @@ public class Tokenizer {
     public static List<Tokenized> tokenize(List<String> stateNames, List<String> spelling) {
         input = new ArrayList<>();
 
+        boolean isOneLine = false;
+        boolean ifFlag = false;
+        boolean isNotBracketed = true;
+        boolean elseFlag = false;
+
         for (int i = 0; i < stateNames.size(); i++) {
+            if (i > 0) {
+                // deal when one-line-if
+                if (spelling.get(i).equals("if")) {
+                    ifFlag = true;
+                }
+                if (ifFlag && spelling.get(i - 1).equals(")") && isNotBracketed) {
+                    isOneLine = !spelling.get(i).equals("{");
+                    if (isOneLine) {
+                        input.add(new Tokenized("{", "{"));
+                        isNotBracketed = false;
+                    }
+                }
+                if (ifFlag && spelling.get(i - 1).equals(";") && isOneLine) {
+                    input.add(new Tokenized("}", "}"));
+                    ifFlag = false;
+                    isOneLine = false;
+                    isNotBracketed = true;
+                }
+
+                // deal when one-line-else
+                if (spelling.get(i - 1).equals("else") && !spelling.get(i).equals("{")) {
+                    input.add(new Tokenized("{", "{"));
+                    isNotBracketed = false;
+                    elseFlag = true;
+                    isOneLine = true;
+                }
+                if (elseFlag && spelling.get(i - 1).equals(";") && isOneLine) {
+                    input.add(new Tokenized("}", "}"));
+                    elseFlag = false;
+                    isOneLine = false;
+                    isNotBracketed = true;
+                }
+            }
+
             if (stateNames.get(i).equals("Comment")) {
                 continue;
             }
